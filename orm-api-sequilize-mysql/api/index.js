74b8d07b894bb3ba4e2c5c04a3950dfff10e6494
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-08-23 18:50:24 -0300

    CRUD associações e controllers

--- orm-api-sequilize-mysql/api/index.js
+++ orm-api-sequilize-mysql/api/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
-
-app.use(bodyParser.json());
-
 const port = 3000;
 
-app.get('/',(req, res) => 
-res
-.status(200)
-.send({mensagem:'Boas-vindas a API rest'}))
+routes(app);
+
 
 app.listen(port, () => console.log(`O servidor está rodando na porta localhost:${port}`));
+
+module.exports = app;
