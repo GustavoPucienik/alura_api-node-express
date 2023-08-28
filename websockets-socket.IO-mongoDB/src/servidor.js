a3ae03d4e31f545539b968422d854fb0eb34299b
Gustavo Gomes Pucienik <gomespucienik@gmail.com>
2023-08-28 00:13:06 -0300

    Projeto Aluradocs: comunicação em tempo real, utilizando socket.io e mongoDB

---
+++ websockets-socket.IO-mongoDB/src/servidor.js
@@ -0,0 +1,24 @@
+import express from "express";
+import url from "url";
+import path from "path";
+import http from "http";
+import { Server } from "socket.io";
+
+import "./dbConnect.js";
+
+const app = express();
+const port = process.env.porta || 3000;
+
+const caminhoAtual = url.fileURLToPath(import.meta.url);
+const diretorioPublico = path.join(caminhoAtual, "../..", "public");
+app.use(express.static(diretorioPublico));
+
+const servidorHttp = http.createServer(app);
+
+servidorHttp.listen(port,  () => console.log(`O servidor está rodando na porta localhost:${port}`));
+
+const io = new Server(servidorHttp);const arr = ['a', 'b', 'c'];
+
+
+
+export default io;
